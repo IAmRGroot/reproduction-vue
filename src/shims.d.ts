89c509d07e55e857808411c0e4e473745f918856
IAmRGroot <github@rgroot.nl>
2022-01-18 14:17:41 +0000

    Added shims

---
+++ src/shims.d.ts
@@ -0,0 +1,12 @@
+declare module '*.vue' {
+    import Vue from 'vue';
+    export default Vue;
+}
+// workaround for Volar to infer the ref type in <template>
+// https://github.com/johnsoncodehk/volar/issues/404
+declare module '@vue/runtime-dom' {
+    export * from '@vue/runtime-dom/dist/runtime-dom';
+    export {
+        defineComponent, PropType, ObjectDirective, FunctionDirective,
+    } from '@vue/composition-api';
+}
