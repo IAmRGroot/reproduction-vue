f5d7243b735fea84ace6345490c9ba8a5f37464d
IAmRGroot <github@rgroot.nl>
2022-05-17 07:03:26 +0000

    Added working case

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
     <div>
+        <!-- {{ json }} -->
         {{ JSON.stringify({ test: 'test'}) }}
     </div>
 </template>
 
 <script lang="ts" setup>
-// 
+// import { computed } from '@vue/composition-api';
+
+// const json = computed(() => JSON.stringify({ test: 'test'}))
 </script>
